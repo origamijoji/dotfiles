6de8e0565819a0d36c127da06a92a9ccd296f514
origamijoji <hello@origamijoji.com>
2025-03-24 06:27:16 -0700

    wezterm, i3

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(window)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
+	gui_window:maximize()
+end)
 
 config.color_scheme = "Catppuccin Macchiato"
 
