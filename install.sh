85495ce80dccce487ea501c123d0c9fd421646b0
origamijoji <47436262+origamijoji@users.noreply.github.com>
2025-08-29 14:39:24 -0700

    feat: lazygit

--- install.sh
+++ install.sh
@@ -77,6 +77,14 @@ if ! command -v eza &>/dev/null; then
   sudo apt install -y eza
 fi
 
+if ! command -v lazygit &>/dev/null; then
+  log "Installing lazygit..."
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+fi
+
 if [ "$SHELL" != "$(command -v zsh)" ]; then
   chsh -s "$(command -v zsh)"
 fi
