34a9b266c6d9aea972723659ad8bbd4767fbd0e0
origamijoji <47436262+origamijoji@users.noreply.github.com>
2025-08-29 19:50:13 -0700

    feat: override colorscheme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    tag = "v1.10.0", -- LazyVim/LazyVim issue #6355
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
